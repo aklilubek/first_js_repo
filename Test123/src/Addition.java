4c3efc1af070255779bd32ccb5f1b0e3b0a76eb3
aklilubek <aklilubek2016@gmail.com>
2020-04-01 07:27:57 -0700

    test comm

---
+++ Test123/src/Addition.java
@@ -0,0 +1,16 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x  = 10;
+		int  y=12;
+		
+		int z=x+y;
+		
+	 System.out.println("This is addition of x+y:"  + z);
+
+	}
+
+}
---
+++ Test123/src/Multiplication.java
@@ -0,0 +1,16 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=44;
+		int y=555;
+		int z=x*y;
+		
+		System.out.println(z);
+		
+		
+	}
+
+}
